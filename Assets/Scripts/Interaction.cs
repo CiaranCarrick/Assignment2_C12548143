2d79ed10b0015d435b54f542629ebf299a572717
CiaranCarrick <carricker16@hotmail.com>
2015-03-10 06:09:53 +0000

    Added Scripts

---
+++ Assets/Scripts/Interaction.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interaction : MonoBehaviour {
+
+	public static float myro;
+	public GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Interact ();
+	}
+	
+	void Interact() {
+		if (Input.GetKeyDown(KeyCode.E)) 
+		{
+			RaycastHit hit;
+			if (Physics.Raycast(transform.position, transform.forward, out hit, 3f))
+			{
+				//if (hit.collider.GetComponent<KeyCards>()!=null) //Check if object has Keycards Script
+				//{
+					if (hit.collider.GetComponent<RotateCube>()!=null){ //Check if object has Keycards Script
+					Debug.Log(hit.collider.GetComponent<RotateCube>().currentState);
+						//{
+						if(hit.collider.GetComponent<RotateCube>().currentState==RotateCube.States.Sleep)
+						{
+						hit.collider.GetComponent<RotateCube>().currentState=RotateCube.States.Rotate;
+						}
+					}
+				if (hit.collider.gameObject.name==("Door") &&myro==90f){
+					Destroy(hit.collider.gameObject);
+				}
+				if (hit.collider.gameObject.name==("Door") &&myro!=90f){
+					cube.GetComponent<RotateCube>().currentState=RotateCube.States.Initialize;
+				}
+
+			}
+		}//end input
+	}//end interact
+}
---
+++ Assets/Scripts/RotateCube.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCube : MonoBehaviour {
+	public Transform turret, player;
+	float shootTime, shootTimer = 0.3f;
+	float searchTime, searchTimer = 4f;
+	float randLookTime, randLookTimer = 0.3f;
+	Quaternion rotation = Quaternion.identity;
+	private float angle;
+	public float seconds = 0.2f;
+	public bool rotating;
+
+	public enum States //our states
+	{
+		Initialize,
+		Sleep,
+		Rotate,
+	}
+	public States currentState = States.Initialize; //create an instance of the enum and set it's default to Initialize
+	void Update ()
+	{
+		switch(currentState) //pass in the current state
+		{
+		case States.Initialize:
+			Initilize();
+			break;
+		case States.Sleep:
+			Sleep ();
+			break;
+		case States.Rotate:
+			Rotate ();
+			break;
+		}
+		//Debug.Log (currentState);
+		Debug.DrawRay(turret.position, turret.transform.forward * 10f, Color.magenta);
+	}
+	void Initilize()
+	{
+		transform.rotation = Quaternion.identity;
+		turret = GameObject.Find ("RotationCube").transform;
+		player = GameObject.Find ("Player").transform;
+
+		currentState = States.Sleep;
+	}
+	void Sleep()
+	{
+		if(Vector3.Distance(turret.position, player.position) > 4f)
+		{
+			currentState = States.Initialize;
+		}
+	}
+	void Rotate()
+	{
+		StartCoroutine(Rotateme(Vector3.up*90));
+
+	}
+	
+	IEnumerator Rotateme(Vector3 degrees)
+	{
+		if (rotating) {
+			return true;
+		}
+		rotating = true;
+		float time = 0.0f;
+		float rate = 1.0f /seconds;
+		Quaternion startRotation = this.transform.rotation;
+		Quaternion endRotation = this.transform.rotation * Quaternion.Euler (degrees);
+		while (time<1.0f) {
+			time += Time.deltaTime * rate;
+			transform.rotation = Quaternion.Slerp (startRotation, endRotation, time);
+			yield return 0;
+		}
+		print("Reached the target.");
+		rotating = false;
+		currentState = States.Sleep;
+	}
+} 
